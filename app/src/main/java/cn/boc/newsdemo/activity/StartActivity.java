99ced44bbbdfc38618b205ee33bd055bde27fe30
wanglj <372184603@qq.com>
2016-07-20 10:00:16 +0800

    新闻客户端示例工程

---
+++ app/src/main/java/cn/boc/newsdemo/activity/StartActivity.java
@@ -0,0 +1,61 @@
+package cn.boc.newsdemo.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.boc.newsdemo.R;
+
+public class StartActivity extends AppCompatActivity {
+
+
+
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            startActivity(new Intent(StartActivity.this,LoginActivity.class));
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+//                Message message =  new Message();
+//                message.what = 2;
+//                message.obj = "发送一条消息";
+//                handler.sendMessage(message);
+
+                handler.sendEmptyMessage(2);
+
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        startActivity(new Intent(StartActivity.this,LoginActivity.class));
+//                    }
+//                });
+
+            }
+        }).start();
+
+
+
+
+
+    }
+}
